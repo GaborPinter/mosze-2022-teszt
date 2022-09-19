45542874191b21a4c49a72e3ff12c6158679d2a0
GaborPinter <pinterg111@gmail.com>
2022-09-19 21:50:53 +0200

    with comments

--- main.cpp
+++ main.cpp
@@ -4,18 +4,18 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS];    // hiányzik az aláhúzás karakter
+    std::cout << '1-100 ertekek duplazasa'      // kettõ aposztróf közé kell írni a szöveget, hiányzik a pontos vesszõ
+    for (int i = 0;)       // nincs befejezve a for ciklus
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++)     // i<N_ELEMENTS
     {
-        std::cout << "Ertek:"
-    }    
+        std::cout << "Ertek:"   // hiányzik a változó és a pontos vesszõ is
+    }
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
+    int atlag;      // nincs kezdõérték megadva
     for (int i = 0; i < N_ELEMENTS, i++)
     {
         atlag += b[i]
